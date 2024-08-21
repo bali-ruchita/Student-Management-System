b0b13833231e0bc557a049ba3d1153a91e49a7be
baliruchitha2004@gmail.com <Bali Ruchita>
2024-08-21 15:14:11 +0530

    Project added

---
+++ src/test/java/com/studentmanagment/StudentManagmentSystem/StudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentmanagment.StudentManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
